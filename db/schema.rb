2e1a523b847fd696fd30c89bd943b7a14f8705c8
Low Yeong Yih <low.yeongyih@hotmail.com>
2016-08-04 17:57:07 +0800

    4th day: CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20160803071352) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "topic_id"
   end
 
 end
